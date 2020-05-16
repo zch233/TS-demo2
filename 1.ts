005bc16e1788d167939881eaaeb7687c1a73e689
zhangchao <529743595@qq.com>
2020-05-16 22:11:37 +0800

    索引类型

--- 1.ts
+++ 1.ts
@@ -1,2 +1,12 @@
-let a:number = 12231
-console.log(a)
+// 索引类型
+// function plus <T>(obj:T, keys: Array<keyof T>): Array<T[keyof T]> {
+//   console.log(keys.map(v => obj[v]))
+//   return keys.map(v => obj[v])
+// }
+// 等价于
+function plus <T, K extends keyof T>(obj:T, keys: Array<K>): Array<T[K]> {
+  console.log(keys.map(v => obj[v]))
+  return keys.map(v => obj[v])
+}
+
+plus({name: 'zch', age: 15, xx: 123}, ['name', 'age'])
